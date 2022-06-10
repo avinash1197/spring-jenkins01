6110f8c194458ffc06cd271ab7504c736b22e3e2
avinash <avinashzaware4@gmail.com>
2022-06-10 18:59:24 +0530

    second commit

--- src/main/java/com/mongo/SpringJenkins01Application.java
+++ src/main/java/com/mongo/SpringJenkins01Application.java
@@ -10,6 +10,10 @@ public class SpringJenkins01Application {
 
 	static Logger log = LoggerFactory.getLogger(SpringJenkins01Application.class);
 	
+	public void init() {
+		log.info("My App In Init Method");
+	}
+	
 	public static void main(String[] args) {
 		log.info("My App Executing...........");
 		SpringApplication.run(SpringJenkins01Application.class, args);
