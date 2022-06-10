ca7e2f057ab384b910fafaab060cc07a9e446bd0
avinash <avinashzaware4@gmail.com>
2022-06-10 16:20:49 +0530

    First Commit

---
+++ src/main/java/com/mongo/SpringJenkins01Application.java
@@ -0,0 +1,18 @@
+package com.mongo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkins01Application {
+
+	static Logger log = LoggerFactory.getLogger(SpringJenkins01Application.class);
+	
+	public static void main(String[] args) {
+		log.info("My App Executing...........");
+		SpringApplication.run(SpringJenkins01Application.class, args);
+	}
+
+}
